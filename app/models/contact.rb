3f6ff4e89fa03db2e959c898be62741112cbc12a
Smooke09 <pedrolucas1014zz@gmail.com>
2022-10-07 14:14:56 -0300

    CORS

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,49 +1,53 @@
 class Contact < ApplicationRecord
-    #Associations
-    belongs_to :kind
+
+    # Associations
+    belongs_to :kind #, optional: true
     has_many :phones
-    has_one :adress
-    
+    has_one :address
+  
     accepts_nested_attributes_for :phones, allow_destroy: true
-    accepts_nested_attributes_for :adress, update_only: true
-
+    accepts_nested_attributes_for :address, update_only: true
+  
+  
     def as_json(options={})
-    h = super(options)
-    h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
-    h
-end
-
-
+      h = super(options)
+      h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+      h
+    end
+  
     # def birthdate_br
-    #     I18n.l(self.birthdate) unless self.birthdate.blank?
+    #   I18n.l(self.birthdate) unless self.birthdate.blank?
     # end
-
+  
     # def to_br
-    #     {
-    #         name: self.name,
-    #         email: self.email,
-    #         birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
-    #     }
+    #   {
+    #     name: self.name,
+    #     email: self.email,
+    #     birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    #   }
     # end
-    # belongs_to :kind, optional: true
-
+  
     # def author
-    #     "Pedro lucas"
+    #   "Jackson Pires"
     # end
-
+  
     # def kind_description
-    #     self.kind.description
+    #   self.kind.description
     # end
-
-    # def as_json(options = {})
-    #     super(
-    #         root: true,
-    #         methods: [:kind_description, :author,], 
-
-    #     )
+  
+    # def as_json(options={})
+    #   super(
+    #     root: true,
+    #     methods: [:kind_description, :author],
+    #     include: { kind: { only: :description }}
+    #   )
     # end
-
-    # def hello 
-    #     I18n.t('hello')
+  
+    # def hello
+    #   I18n.t('hello')
     # end
-end
+  
+    # def i18n
+    #   I18n.default_locale
+    # end
+  end
